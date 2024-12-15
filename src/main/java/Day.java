6bea2f0677ba85b76d85e106afea2ea60dee9c4b
Mikolaj Przygoda <przygoda.mikolaj@gmail.com>
2024-12-15 19:03:55 +0100

    Solve Day 2 cont.

--- src/main/java/Day.java
+++ src/main/java/Day.java
@@ -1,4 +1,7 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public abstract class Day {
@@ -6,11 +9,12 @@ public abstract class Day {
         return getResourceLines("aocData/" + name);
     }
 
+    @SuppressWarnings("unused")
     static protected List<String> getTestData(String name) {
         return getResourceLines("aocData/" + name + "-test");
     }
 
-    static private List<String> getResourceLines(String resourceName){
+    static private List<String> getResourceLines(String resourceName) {
         try (InputStream inputStream = Day.class.getClassLoader().getResourceAsStream(resourceName)) {
             assert inputStream != null : "Resource \"" + resourceName + "\" should be opened to read";
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
@@ -20,13 +24,13 @@ public abstract class Day {
         }
     }
 
-    static protected void printPart1(String result){
+    static protected void printPart1(String result) {
         System.out.println("============== < Part 1 > ==============");
         System.out.println(result);
         System.out.println("============== </Part 1 > ==============");
     }
 
-    static protected void printPart2(String result){
+    static protected void printPart2(String result) {
         System.out.println("============== < Part 2 > ==============");
         System.out.println(result);
         System.out.println("============== </Part 2 > ==============");
